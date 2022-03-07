3b7c33cdd80da84de148c167e2047fdf571acc7f
Kevin Wang <kevqwang@gmail.com>
2022-03-06 19:04:59 -0500

    initial appbar

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AppBar, Typography } from '@mui/material/';
+
+const Header = () => {
+  return (
+    <>
+    <AppBar>
+        <Typography>Project</Typography>
+    </AppBar>
+    </>
+  )
+}
+
+export default Header

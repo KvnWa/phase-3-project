601ce93eec5a7cb5a43563a91bd6df38c017278f
Kevin Wang <kevqwang@gmail.com>
2022-03-06 19:29:45 -0500

    initial appbar style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import { AppBar, Typography } from '@mui/material/';
+import React, {useState} from 'react'
+import { AppBar, Typography, Toolbar, Tabs, Tab, Button } from '@mui/material/';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header = () => {
+
+    const [ value, setValue ] = useState();
+
   return (
     <>
-    <AppBar>
-        <Typography>Project</Typography>
+    <AppBar style={{background: '#063970'}}>
+        <Toolbar>
+            <Typography>Project</Typography>
+            <ShoppingCartIcon></ShoppingCartIcon>
+            <Tabs style={{marginLeft: 'auto'}} textColor="inherit" value={value} onChange={(e, value)=> setValue(value)} indicatorColor="primary">
+                <Tab label="Watchlist"></Tab>
+                <Tab label="Preorder"></Tab>
+                <Tab label="Platform"></Tab>
+            </Tabs>
+            <Button style={{marginLeft: 'auto'}} variant="contained">Login</Button>
+            <Button style={{marginLeft: '10px'}} variant="contained">Sign Up</Button>
+        </Toolbar>
     </AppBar>
     </>
   )

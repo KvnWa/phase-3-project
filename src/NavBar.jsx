9c8b6e31b96752bd40f8d6b013edbc00b918f66f
Kevin Wang <kevqwang@gmail.com>
2022-03-09 17:01:50 -0500

    nav progress

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,30 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { AppBar, Typography, Toolbar, Tabs, Tab, Button } from '@mui/material/';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-
-    const [ value, setValue ] = useState();
+  const [value, setValue] = useState();
 
   return (
-    <>
-    <AppBar style={{background: '#063970'}}>
+    <div className="nav">
+      <AppBar style={{ background: '#063970' }}>
         <Toolbar>
-            <Typography>Logo</Typography>
-            <ShoppingCartIcon></ShoppingCartIcon>
-            <Tabs style={{marginLeft: 'auto'}} textColor="inherit" value={value} onChange={(e, value)=> setValue(value)} indicatorColor="primary">
-                <Tab label="Watchlist" to='/watchlist' component={Link}></Tab>
-                <Tab label="About" to='/About' component={Link}></Tab>
-                <Tab label="FAQ" to='/FAQ' component={Link}></Tab>
-            </Tabs>
-            <Button style={{marginLeft: 'auto'}} variant="contained">Login</Button>
-            <Button style={{marginLeft: '10px'}} variant="contained">Sign Up</Button>
+          <Typography to="/" component={Link}>
+            Logo
+          </Typography>
+          <ShoppingCartIcon></ShoppingCartIcon>
+          <Tabs
+            style={{ marginLeft: 'auto' }}
+            textColor="inherit"
+            value={value}
+            onChange={(e, value) => setValue(value)}
+            indicatorColor="primary"
+          >
+            <Tab label="Watchlist" to="/watchlist" component={Link}></Tab>
+            <Tab label="FAQ" to="/FAQ" component={Link}></Tab>
+            <Tab label="About" to="/About" component={Link}></Tab>
+          </Tabs>
+          <Button style={{ marginLeft: 'auto' }} variant="contained">
+            Login
+          </Button>
+          <Button style={{ marginLeft: '10px' }} variant="contained">
+            Sign Up
+          </Button>
         </Toolbar>
-    </AppBar>
-    </>
-  )
-}
+      </AppBar>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;

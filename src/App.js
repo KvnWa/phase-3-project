df2337c99eab244962533eeab0733f0ca39a4dd0
Kevin Wang <kevqwang@gmail.com>
2022-03-09 17:08:10 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import Home from "./Home";
-import NavBar from "./NavBar";
-import About from "./About";
-import FAQ from "./FAQ";
-import GameCard from "./GameCard";
-import Watchlist from "./Watchlist";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import About from './components/About';
+import FAQ from './components/FAQ';
+import GameDetails from './components/GameDetails';
+import Watchlist from './components/Watchlist';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
 
 function App() {
   return (
     <Router>
-    <>
-    <NavBar />
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/watchlist">
-        <Watchlist />
-      </Route>
-      <Route exact path="/FAQ">
-        <FAQ />
-      </Route>
-      <Route exact path="/About">
-        <About />
-      </Route>
-      
-    </Switch>
-    </>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/watchlist">
+          <Watchlist />
+        </Route>
+        <Route exact path="/FAQ">
+          <FAQ />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/games/:id" component={GameDetails} />
+      </Switch>
     </Router>
   );
 }
--- src/Watchlist.jsx
+++ src/Watchlist.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import '../App.css';
+
+import GameCard from './GameCard';
 
 const Watchlist = () => {
-  return (
-    <div>Watchlist
-    </div>
-  )
-}
+  const [games, setGames] = useState([]);
+  const [filteredGames, setFilteredGames] = useState([]);
 
-export default Watchlist
+  useEffect(() => {
+    fetch('http://localhost:3001/watchlist')
+      .then((r) => r.json())
+      .then((data) => {
+        setGames(data);
+        setFilteredGames(data);
+      });
+  }, []);
+
+  const handleButtonClick = (gameId) => {
+    const filteredGames = games.filter((game) => game.id !== gameId);
+    setFilteredGames(filteredGames);
+  };
+
+  return (
+    <div className="container">
+      {filteredGames.map((game) => (
+        <GameCard
+          key={game.id}
+          game={game}
+          isWatchlist={true}
+          handleButtonClick={handleButtonClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Watchlist;

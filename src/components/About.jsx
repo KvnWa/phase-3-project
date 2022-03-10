0cad9c84ab687a01dc2cd781b52e5455200124d3
Kevin Wang <kevqwang@gmail.com>
2022-03-09 19:35:52 -0500

    third css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='about'>
-      <div className='parent'>
-      <strong>Frequently Asked Questions</strong>
-      </div>
-      <br></br>
-      <img src="https://www.cityu.edu/wp-content/uploads/2022/02/we-stand-with-ukraine-1080x617.png"/>
-      
-      
+    <div className="dyn-height">
+          <video width="100%" height="auto" autoplay="" muted="" loop="true" autofocus="" poster="https://cdn.akamai.steamstatic.com/store/about/videos/about_hero_loop_web.png">
+            <source src="https://cdn.akamai.steamstatic.com/store/about/videos/about_hero_loop_web.webm" type="video/webm"/>
+              <source src="https://cdn.akamai.steamstatic.com/store/about/videos/about_hero_loop_web.mp4" type="video/mp4"/>
+              </video>
     </div>
-  )
+      )
 }
 
-export default About
+      export default About
